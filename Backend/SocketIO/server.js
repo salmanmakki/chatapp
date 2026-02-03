6b462d01dd20de68f14b853a6a31169917b94ff2
salman makki <136706274+salmanmakki@users.noreply.github.com>
2026-02-03 14:37:50 +0530

    Update server.js

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -8,11 +8,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // ✅ Vite frontend
-    methods: ["GET", "POST"],
+    origin: [
+      "http://localhost:5173",
+      "https://chatapp-frontend-ten-opal.vercel.app",
+    ],
     credentials: true,
   },
 });
+
 
 const users = {}; // userId -> socketId map
 
